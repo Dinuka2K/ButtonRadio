f794fc76deef7d178232b9f01fbf6087569ea60d
Dinuka2K <dinuka_2020693@fot.ruh.ac.lk>
2023-03-03 16:41:21 +0530

    make new project

---
+++ ButtonGroup/src/main/java/com/mycompany/buttongroup/ButtonGroup.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.buttongroup;
+
+/**
+ *
+ * @author HP
+ */
+public class ButtonGroup {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
